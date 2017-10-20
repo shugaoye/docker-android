dda24f7514aaf83b598a0f155c3ca5aa0e738b02
Roger Ye <shugaoye@yahoo.com>
2017-10-20 08:39:07 +0800

    Changed fix-permission.sh.

--- scripts/fix-permission.sh
+++ scripts/fix-permission.sh
@@ -7,10 +7,16 @@ chmod g+x android ddms emulator* draw9patch hierarchyviewer jobb lint mksdcard m
 chmod g+x monkeyrunner screenshot2 traceview uiautomatorviewer
 
 cd ${ANDROID_SDK_HOME}/build-tools/
-chmod g+x *
+chmod g+w *
 
 cd ${ANDROID_SDK_HOME}/platforms
-chmod g+x *
+chmod g+w *
+
+chmod g+w ${ANDROID_SDK_HOME}/extras/android/*
+chmod g+w ${ANDROID_SDK_HOME}/extras/google/*
+chmod g+w ${ANDROID_SDK_HOME}/platform-tools
+chmod g+w ${ANDROID_SDK_HOME}/tools
+chmod g+w ${ANDROID_SDK_HOME}/system-images/android-24/default/*
 
 chmod g+w ${ANDROID_SDK_HOME}/.android
 
